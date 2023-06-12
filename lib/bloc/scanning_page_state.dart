8af7cc2bfbe27c61524e39a278673c1d0d934b39
sajna nazar <sajna.nazar@marketsimplified.com>
2023-06-12 19:44:37 +0530

    updated

--- lib/bloc/scanning_page_state.dart
+++ lib/bloc/scanning_page_state.dart
@@ -1,20 +1,3 @@
-// class ScanningPageState extends Equatable {
-//   final String? imageData;
-//   final String? error;
-
-//   const ScanningPageState({this.imageData, this.error});
-
-//   ScanningPageState copyWith({String? imageData, String? error}) {
-//     return ScanningPageState(
-//       imageData: imageData ?? this.imageData,
-//       error: error ?? this.error,
-//     );
-//   }
-
-//   @override
-//   List<Object?> get props => [imageData];
-// }
-
 class ScanningPageState {
   final String? imageData;
   final String? error;
